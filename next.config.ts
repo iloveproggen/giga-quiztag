982f6ab71c7ace27c095661d92033e60cb58f105
Michelle Gerwald <149069955+iloveproggen@users.noreply.github.com>
2026-08-03 13:33:05 +0200

    export changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  allowedDevOrigins: ["192.168.0.151"],
 };
 
 export default nextConfig;
